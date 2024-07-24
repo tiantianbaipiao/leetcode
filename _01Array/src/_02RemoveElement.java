6e8d42ec30d9f8cca99494a39bd61366d133daa8
root <1241062101@qq.com>
2024-07-24 19:46:29 +0800

    RemoveElement

---
+++ _01Array/src/_02RemoveElement.java
@@ -0,0 +1,23 @@
+/**
+ * 删除元素
+ */
+public class _02RemoveElement {
+  // 快慢指针法
+  public static int removeElement(int[] nums, int val) {
+    // 定义慢指针
+    int slow = 0;
+    // 遍历数组
+    for (int fast = 0; fast < nums.length; fast++){
+      if(nums[fast] != val){
+        nums[slow] = nums[fast];
+        slow++;
+      }
+    }
+    return slow;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {0,1,2,2,3,0,4,2};
+    System.out.println(removeElement(nums, 2));
+  }
+}
