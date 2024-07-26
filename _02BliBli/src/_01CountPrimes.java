93c7f46eb6b5bf070df252c84f027c9ac637b083
root <1241062101@qq.com>
2024-07-26 21:29:06 +0800

    Add two methods to count prime numbersImplement two methods to calculate the number of prime numbers within a given range: 1. A brute force approach that checks each number for primality. 2. An optimized approach using the Sieve of Eratosthenes algorithm.
    Also, include a main method for testing these implementations.

---
+++ _02BliBli/src/_01CountPrimes.java
@@ -0,0 +1,55 @@
+/**
+ * 计算质数的个数
+ */
+public class _01CountPrimes {
+  // 1.暴力解法
+  public int countPrimes_01(int n) {
+    int count = 0;
+    if ( n == 0 || n == 1 ) {
+      return 0;
+    }
+    for ( int i = 2; i < n; i++){
+      if(isPrimes(i)){
+        count++;
+      }
+    }
+    return count;
+  }
+
+  private boolean isPrimes(int i) {
+    for (int j = 2; j < i; j++){
+      if(i % j == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // 埃式筛选(标记法)
+  public int countPrimes_02(int n) {
+    // 1. 定义一个布尔类型数组用于标记数字是否为质数
+    boolean[] isPrimes = new boolean[n]; // 默认为false 表示质数
+    // 2. 定义计数器
+    int count = 0;
+    // 3.判断特殊情况
+    if ( n == 0 || n == 1 ) {
+      return 0;
+    }
+    // 4.遍历
+    for ( int i = 2; i < n; i++){
+      if(!isPrimes[i]){
+        count++;
+        for ( int j = i; j < n; j += i){
+          isPrimes[j] = true;
+        }
+      }
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    _01CountPrimes countPrimes = new _01CountPrimes();
+    System.out.println(countPrimes.countPrimes_01(10));
+    System.out.println(countPrimes.countPrimes_02(10));
+  }
+}
