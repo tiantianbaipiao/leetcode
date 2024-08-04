3d85dfeeed2f9c85b9ddf555521b0bfb2196d563
root <1241062101@qq.com>
2024-08-04 23:02:14 +0800

    feat(list-node): 添加合并两个有序链表的功能实现一个新的`_06MergeTwoLists`类，该类通过交替节点合并两个排序链表。 此外，还定义了一个新的`ListNode`实用程序类，用于链表节点的创建和操作。
    此举旨在提供一个通用的解决方案，以处理合并两个有序链表的需求。

---
+++ _02BliBli/src/_06MergeTwoLists.java
@@ -0,0 +1,20 @@
+import utils.ListNode;
+
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+public class _06MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
