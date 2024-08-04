335e66c05ef1f769dd2b19b5c4928a0ec5bf7c11
root <1241062101@qq.com>
2024-08-04 22:16:44 +0800

    ```新增 _02ClimbStairs 类，提供爬楼梯问题的解决方案
    添加了一个新的 _02ClimbStairs 类，其中包含一个方法 climbStairs_01，用于解决爬楼梯问题。该方法通过动态规划计算到达第 n 个台阶的不同方法数量。还添加了一个 main 方法用于测试。```

---
+++ _02BliBli/src/_02ClimbStairs.java
@@ -0,0 +1,39 @@
+/**
+ * 70. 爬楼梯
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ */
+public class _02ClimbStairs {
+    public int climbStairs_01(int n) {
+        if (n == 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        // 一个梯子有1种爬法
+        // 两个梯子有2种爬法
+        // 三个梯子有3种爬法
+        // 四个梯子有5种爬法
+        // 五个梯子有8种爬法
+        // 对于第i个台阶，方法数等于到达第i-1个台阶的方法数加上到达第i-2个台阶的方法数
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+  public static void main(String[] args) {
+    _02ClimbStairs cs = new _02ClimbStairs();
+    System.out.println(cs.climbStairs_01(5));
+  }
+}
