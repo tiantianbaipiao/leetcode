4f0a4c4ea18fa93b9b03bc285ebfb4cc9dd649c2
root <1241062101@qq.com>
2024-08-07 17:27:56 +0800

    ```plaintext添加中序遍历实现及调整TreeNode类
    - 实现了二叉树的中序遍历算法，并将结果以List<Integer>的形式返回。
    - 对utils包下的TreeNode类进行了调整，确保其属性可见性与实际需求一致。
    ```

---
+++ _02BliBli/src/_14InorderTraversal.java
@@ -0,0 +1,38 @@
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class _14InorderTraversal {
+  // 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+
+    // 直接返回空列表，如果根节点为空
+    if (root == null) {
+      return result;
+    }
+
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode currentNode = root;
+
+    // 循环直到当前节点为空并且栈也为空
+    while (currentNode != null || !stack.isEmpty()) {
+      // 将左子树压入栈中
+      while (currentNode != null) {
+        stack.push(currentNode);
+        currentNode = currentNode.left;
+      }
+
+      // 弹出栈顶元素，并将其值加入结果列表
+      currentNode = stack.pop();
+      result.add(currentNode.val);
+
+      // 移动到右子树
+      currentNode = currentNode.right;
+    }
+    return result;
+  }
+
+}
