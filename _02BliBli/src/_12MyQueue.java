778a997b2f07df50b3f427bb9df61b6c4aae7299
root <1241062101@qq.com>
2024-08-07 17:09:53 +0800

    实现MyQueue类以展示使用两个栈实现队列的方法

---
+++ _02BliBli/src/_12MyQueue.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class _12MyQueue {
+  // 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）
+  public static class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+    public MyQueue() {
+      stack1 = new Stack<>();
+      stack2 = new Stack<>();
+    }
+    public void push(int x) {
+      stack1.push(x);
+    }
+    public int pop() {
+      if (stack2.isEmpty()) {
+        while (!stack1.isEmpty()) {
+          stack2.push(stack1.pop());
+        }
+      }
+      return stack2.pop();
+    }
+    public int peek() {
+      if (stack2.isEmpty()) {
+        while (!stack1.isEmpty()) {
+          stack2.push(stack1.pop());
+        }
+      }
+      return stack2.peek();
+    }
+    public boolean empty() {
+      return stack1.isEmpty() && stack2.isEmpty();
+    }
+  }
+}
