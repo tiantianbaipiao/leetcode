86baf3d7e6a6a2bbaa6a0257504ed7f1194abf85
root <1241062101@qq.com>
2024-08-08 22:26:19 +0800

    实现二叉树平衡性判断算法
    新增`_19IsBalanced.java`文件，其中包含一个`isBalanced`方法用于判断给定二叉树是否是平衡二叉树。此方法通过递归计算树的左右子树深度，并判断其差值是否不大于1来实现平衡性的判断。同时，新增`maxDepth`方法用于计算二叉树的最大深度。

---
+++ _02BliBli/src/_19IsBalanced.java
@@ -0,0 +1,17 @@
+import utils.TreeNode;
+
+public class _19IsBalanced {
+  // 给定一个二叉树，判断它是否是平衡二叉树
+  public boolean isBalanced(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+  }
+  private int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
+}
