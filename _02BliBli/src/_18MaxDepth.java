bb7ca011ad1a68627aaaa669b3ce24657c73c8b5
root <1241062101@qq.com>
2024-08-08 22:23:58 +0800

    添加18MaxDepth.java文件，实现二叉树最大深度的计算

---
+++ _02BliBli/src/_18MaxDepth.java
@@ -0,0 +1,11 @@
+import utils.TreeNode;
+
+public class _18MaxDepth {
+  // 给定一个二叉树 root ，返回其最大深度。
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
+}
