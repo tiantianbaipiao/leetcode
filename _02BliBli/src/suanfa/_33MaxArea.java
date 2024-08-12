9c532c0948bce3fa9d0506696839539d80392a9a
root <1241062101@qq.com>
2024-08-12 21:33:35 +0800

    ```添加了求解最大水量问题的算法
    实现了计算最大水量的逻辑，使用双指针法在给定的垂线中找出能容纳最多水的两条线。```

---
+++ _02BliBli/src/suanfa/_33MaxArea.java
@@ -0,0 +1,26 @@
+package suanfa;
+
+/**
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ */
+public class _33MaxArea {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            if (area > max) {
+                max = area;
+            }
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+       }
+        return max;
+    }
+}
