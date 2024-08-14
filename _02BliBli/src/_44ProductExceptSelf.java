4e0ce13f4ea93cee39c38e45825dd713f5d4c947
root <1241062101@qq.com>
2024-08-14 20:45:22 +0800

    实现产品除自身问题的解决方案实现了一个遵循O(n)时间复杂度要求的算法，该算法能够计算出每个数组元素对应的所有其他元素的乘积，而不使用除法操作。此实现通过两次遍历数组分别计算左侧和右侧元素的乘积来达到目的。

--- _02BliBli/src/_44ProductExceptSelf.java
+++ _02BliBli/src/_44ProductExceptSelf.java
@@ -4,4 +4,24 @@
  * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
  */
 public class _44ProductExceptSelf {
+  public int[] productExceptSelf(int[] nums) {
+    int length = nums.length;
+    int[] answer = new int[length];
+
+    // 左侧元素的乘积
+    answer[0] = 1;
+    for (int i = 1; i < length; i++) {
+      answer[i] = nums[i - 1] * answer[i - 1];
+    }
+
+    // 右侧元素的乘积
+    int R = 1;
+    for (int i = length - 1; i >= 0; i--) {
+      answer[i] = answer[i] * R;
+      R *= nums[i];
+    }
+
+    return answer;
+  }
 }
+
