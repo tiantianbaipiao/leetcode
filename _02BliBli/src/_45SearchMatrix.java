4074bf62a8d9ab034ade75c6eb5489090a72e6b1
root <1241062101@qq.com>
2024-08-14 20:47:30 +0800

    ```java添加了矩阵搜索算法的实现
    实现了搜索矩阵的高效算法，能够处理空矩阵和目标值不在矩阵中的情况。该算法通过从矩阵右上角开始搜索，并根据当前值与目标值的比较，决定向下或向左移动，直到找到目标值或搜索范围超出矩阵边界。
    ```

---
+++ _02BliBli/src/_45SearchMatrix.java
@@ -0,0 +1,29 @@
+/**
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ *
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ */
+public class _45SearchMatrix {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+      return false;
+    }
+
+    int row = 0;
+    int col = matrix[0].length - 1;
+
+    while (row < matrix.length && col >= 0) {
+      if (matrix[row][col] == target) {
+        return true;
+      } else if (matrix[row][col] < target) {
+        row++; // Move down
+      } else {
+        col--; // Move left
+      }
+    }
+
+    return false;
+  }
+}
+
