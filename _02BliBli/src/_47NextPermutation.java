e22f8be306b86d6f1c0edbab05b6d392838422d1
root <1241062101@qq.com>
2024-08-15 18:25:25 +0800

    ```新增解决方案：leetcode 31. Next Permutation
    实现了一个算法，用于找出给定序列的下一个排列。该算法首先从右向左查找第一个相邻升序对，然后在该位置找到下一个更大的元素并进行交换，最后反转该位置之后的所有元素。```

--- _02BliBli/src/_47NextPermutation.java
+++ _02BliBli/src/_47NextPermutation.java
@@ -1,2 +1,40 @@
+/**
+ * leetcode 31. Next Permutation
+ */
 public class _47NextPermutation {
+  public void nextPermutation(int[] nums) {
+    // Step 1: Find the first element from the right that is smaller than its next element.
+    int i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+      i--;
+    }
+
+    // Step 2: If such an element was found, find the next greater element to the right.
+    if (i >= 0) {
+      int j = nums.length - 1;
+      while (nums[j] <= nums[i]) {
+        j--;
+      }
+      // Step 3: Swap these two elements.
+      swap(nums, i, j);
+    }
+
+    // Step 4: Reverse the part of the array after the index i.
+    reverse(nums, i + 1, nums.length - 1);
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
+  private void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      swap(nums, start, end);
+      start++;
+      end--;
+    }
+  }
 }
+
