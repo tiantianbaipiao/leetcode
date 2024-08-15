d6d0517c52428c5f1b7f47a7cdea17238ef74f8b
root <1241062101@qq.com>
2024-08-15 18:22:26 +0800

    ```markdown```none feat(_46GenerateParenthesis): 添加生成括号组合的类
    实现了一个生成所有可能的有效的括号组合的算法。通过回溯方法，当open和close数量满足条件时添加到结果集中。
    
    feat(_47NextPermutation): 创建NextPermutation类
    
    该类将提供一种方法来获取给定序列的下一个排列。当前该类是一个空类，算法实现将在后续提交中添加。
    ```
    ```

---
+++ _02BliBli/src/_46GenerateParenthesis.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ */
+public class _46GenerateParenthesis {
+  public List<String> generateParenthesis(int n) {
+    List<String> result = new ArrayList<>();
+    backtrack(result, "", 0, 0, n);
+    return result;
+  }
+
+  private void backtrack(List<String> result, String current, int open, int close, int max) {
+    if (current.length() == max * 2) {
+      result.add(current);
+      return;
+    }
+
+    if (open < max)
+      backtrack(result, current + "(", open + 1, close, max);
+    if (close < open)
+      backtrack(result, current + ")", open, close + 1, max);
+  }
+}
+
---
+++ _02BliBli/src/_47NextPermutation.java
@@ -0,0 +1,2 @@
+public class _47NextPermutation {
+}
