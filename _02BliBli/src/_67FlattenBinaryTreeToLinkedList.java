c6ab0fab611e4f1f04b4e0b3b10dc4b255eab086
root <1241062101@qq.com>
2024-08-15 19:16:29 +0800

    ```markdown```git commit e8358b23cc92b1d12c82e2892028d2f63d288532 Author: your_name <your_email> Date:   Wed Feb 2415:41:22 2021 +0800
    将二叉树展开为链表
    
    新增了将二叉树展开为链表的算法实现。通过压平二叉树的左右子树，使其成为一个单链表，该链表遵循二叉树的先序遍历顺序。
    ```
    ```

---
+++ _02BliBli/src/_67FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,32 @@
+import utils.TreeNode;
+
+/**
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ */
+public class _67FlattenBinaryTreeToLinkedList {
+  public void flatten(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+
+    flatten(root.left);
+    flatten(root.right);
+
+    // 保存右子树
+    TreeNode right = root.right;
+
+    // 将左子树移动到右子树的位置
+    root.right = root.left;
+    root.left = null;
+
+    // 连接原右子树到左子树的末端
+    TreeNode p = root;
+    while (p.right != null) {
+      p = p.right;
+    }
+    p.right = right;
+  }
+}
+
