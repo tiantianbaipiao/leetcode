d1534003096754778a9597ed89a54ca58dffc1b8
root <1241062101@qq.com>
2024-08-15 18:49:17 +0800

    ```markdown```none feat(_57UniquePaths): 新建UniquePaths类，计算从左上角到右下角的唯一路径数量
    实现动态规划方法，在给定网格大小(m x n)的情况下，计算机器人从左上角到右下角的唯一路径数量。
    该问题等价于计算从(0, 0)到(m-1, n-1)的路径数量，每次只能向下或向右移动一步。
    ```
    ```

---
+++ _02BliBli/src/_57UniquePaths.java
@@ -0,0 +1,30 @@
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 问总共有多少条不同的路径？
+ */
+public class _57UniquePaths {
+  public int uniquePaths(int m, int n) {
+    if (m == 0 || n == 0) return 0;
+
+    int[][] dp = new int[m][n];
+
+    // 初始化第一行和第一列
+    for (int i = 0; i < m; i++) {
+      dp[i][0] = 1;
+    }
+    for (int j = 0; j < n; j++) {
+      dp[0][j] = 1;
+    }
+
+    // 动态规划填充 dp 数组
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+
+    return dp[m - 1][n - 1];
+  }
+}
+
