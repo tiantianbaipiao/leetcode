d496755844d98928f575b6aff008855b4cce72d8
root <1241062101@qq.com>
2024-08-15 18:42:12 +0800

    ```markdown``` feat(_54MaxSubArray): 实现最大子数组和的解决方案
    根据问题描述，实现了一个名为maxSubArray的函数，该函数接受一个整数数组，
    并返回具有最大和的连续子数组的最大和。使用了动态规划方法来解决该问题，
    算法的时间复杂度为O(n)，其中n是数组的长度。问题描述：
    给定一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），
    返回其最大和。子数组是数组中的一个连续部分。
    
    示例：
    输入：[-2,1,-3,4,-1,2,1,-5,4]
    输出：6解释：连续子数组 [4,-1,2,1] 具有最大和 6。
    ``````

--- _02BliBli/src/_54MaxSubArray.java
+++ _02BliBli/src/_54MaxSubArray.java
@@ -1,2 +1,22 @@
+/**
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 子数组
+ * 是数组中的一个连续部分。
+ */
 public class _54MaxSubArray {
+  public int maxSubArray(int[] nums) {
+    if (nums == null || nums.length == 0) return 0;
+
+    int maxSoFar = nums[0];
+    int currentMax = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      currentMax = Math.max(currentMax + nums[i], nums[i]);
+      maxSoFar = Math.max(maxSoFar, currentMax);
+    }
+
+    return maxSoFar;
+  }
 }
+
