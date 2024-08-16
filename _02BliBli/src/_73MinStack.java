10499a1b971724d35b801eed6c3ac051b12168ab
root <1241062101@qq.com>
2024-08-16 20:29:36 +0800

    ```feat(min-stack): 实现支持getMin操作的栈
    设计并实现一个支持push、pop、top及getMin操作的MinStack类，其中getMin操作能返回栈中最小的元素。push、pop、top和getMin操作均需在常数时间内完成。
    
    使用两个栈：一个主栈（stack）用于存储所有元素，另一个辅助栈（minStack）用于存储当前栈内最小元素。在每次push操作时，如果新元素小于等于辅助栈顶元素，则将其压入辅助栈；在每次pop操作时，检查弹出元素是否为当前最小元素并相应更新辅助栈；top操作直接返回主栈顶元素；getMin操作返回辅助栈顶元素。
    
    如此设计下，所有操作均能在O(1)时间内完成，同时能有效追踪栈内最小元素。
    ```

---
+++ _02BliBli/src/_73MinStack.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+/**
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ * 实现 MinStack 类:
+ *
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ */
+public class _73MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public _73MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
