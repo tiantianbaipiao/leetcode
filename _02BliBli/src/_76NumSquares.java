ce5046818a16f6bbcb07c0855d66fc48ce6e9a9b
root <1241062101@qq.com>
2024-08-17 21:43:55 +0800

    ```javafeat(num-squares): 实现完全平方数的最少数量计算
    添加一个新的类`NumSquares`，其中包含一个方法`numSquares`，用于计算和为给定整数n的完全平方数的最少数量。完全平方数是指可以表示为某个整数的平方的数。此实现使用动态规划算法，通过填充一个dp数组来找出最小数量。
    ```

---
+++ _02BliBli/src/_76NumSquares.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
+ * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。
+ * 例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+ */
+public class _76NumSquares {
+    public int numSquares(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
