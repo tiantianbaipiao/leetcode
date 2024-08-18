52f3a8a919300644772d14a9f966faeb1eda6e09
root <1241062101@qq.com>
2024-08-18 22:36:41 +0800

    refactor(_91FindUnsortedSubarray): 使用排序副本优化寻找无序子数组
    通过创建原始数组的一个排序副本，优化了寻找需要排序的最小子数组的逻辑。
    此改动避免了在每次迭代中寻找全局最大和最小值的需要，简化了算法逻辑，并可能提高了效率。

--- _02BliBli/src/_91FindUnsortedSubarray.java
+++ _02BliBli/src/_91FindUnsortedSubarray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，
  * 那么整个数组都会变为升序排序。
@@ -5,27 +7,26 @@
  */
 public class _91FindUnsortedSubarray {
   public int findUnsortedSubarray(int[] nums) {
-    int start = -1;  // 起始位置
-    int end = -1;    // 结束位置
-    int max = Integer.MIN_VALUE;  // 已遍历部分的最大值
-    int min = Integer.MAX_VALUE;  // 已遍历部分的最小值
+    int[] sortedNums = nums.clone();  // 创建原始数组的一个副本
+    Arrays.sort(sortedNums);  // 对副本进行排序
+
+    int start = -1;
+    int end = -1;
 
     // 寻找起始位置
     for (int i = 0; i < nums.length; i++) {
-      if (nums[i] < max) {
+      if (nums[i] != sortedNums[i]) {
         start = i;
         break;
       }
-      max = nums[i];
     }
 
     // 寻找结束位置
     for (int i = nums.length - 1; i >= 0; i--) {
-      if (nums[i] > min) {
+      if (nums[i] != sortedNums[i]) {
         end = i;
         break;
       }
-      min = Math.min(min, nums[i]);
     }
 
     // 如果数组已经是有序的
@@ -43,4 +44,5 @@ public class _91FindUnsortedSubarray {
     System.out.println("Result: " + result);  // 输出: 5
   }
 }
+
 
