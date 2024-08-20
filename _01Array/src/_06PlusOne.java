061fffde81bc7688b0e621a3e58bf445c63e3a7e
root <1241062101@qq.com>
2024-08-20 22:12:46 +0800

    feat(array): 添加数组形式加一和排序数组转换为BST的类
    添加了两个新的问题解决方案：1. _06PlusOne.java - 实现了在数组表示的非负整数上加一的功能。
    2. _07SortedArrayToBST.java - 构建一个将排序数组转换为平衡二叉搜索树的类。
    
    这些问题都是在数组操作和数据结构转换中的常见编码挑战。

---
+++ _01Array/src/_06PlusOne.java
@@ -0,0 +1,36 @@
+/**
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ *
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ *
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头
+ */
+public class _06PlusOne {
+
+  // 数组形式的数字加一
+  public int[] plusOne(int[] digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+
+    // 处理最高位进位
+    int[] newDigits = new int[digits.length + 1];
+    newDigits[0] = 1;
+    return newDigits;
+  }
+
+  public static void main(String[] args) {
+    _06PlusOne solution = new _06PlusOne();
+    int[] digits = {1, 2, 3};
+    int[] result = solution.plusOne(digits);
+    for (int num : result) {
+      System.out.print(num);
+    }
+    System.out.println();
+  }
+}
+
