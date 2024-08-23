b35524dfc95c0f90ccdcabb7a00c284ce4d667a9
root <1241062101@qq.com>
2024-08-23 19:42:32 +0800

    refactor(_20MinMoves): 重构代码，增加数组元素相等的最小移动次数功能
    重构 _20MinMoves 类，新增 minMoves 方法，该方法计算将数组中所有元素变为相等所需的最小移动次数。
    每次移动可以增加或减少任意一个元素的值。通过排序数组并累计每个元素与最小元素的差值来计算总移动次数。

---
+++ _01Array/src/_20MinMoves.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/**
+ * 最小移动次数使数组元素相等 要求我们计算最少需要多少次操作使得数组中的所有元素相等，
+ * 其中每次操作可以选择任意一个元素并将其增加或减少 1。
+ */
+public class _20MinMoves {
+  public int minMoves(int[] nums) {
+    // 首先对数组进行排序
+    Arrays.sort(nums);
+
+    // 找到数组中的最小值
+    int minValue = nums[0];
+
+    // 初始化移动次数为 0
+    int moves = 0;
+
+    // 遍历数组中的每个元素
+    for (int num : nums) {
+      // 计算当前元素与最小值之间的差值
+      int diff = num - minValue;
+
+      // 移动次数加上差值
+      moves += diff;
+    }
+
+    // 返回计算得到的移动次数
+    return moves;
+  }
+}
+
+
+
