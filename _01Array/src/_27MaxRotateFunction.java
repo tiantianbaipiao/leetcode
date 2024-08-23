d3a1741f743fdc715f1f58754802ac9b74745973
root <1241062101@qq.com>
2024-08-23 19:58:09 +0800

    ```java新建_27MaxRotateFunction类，提供计算最大旋转函数值的解决方案
    _27MaxRotateFunction类中的maxRotateFunction方法能够计算并返回给定数组的各种旋转状态下旋转函数的最大值。此方法通过一次性计算数组的总和和初始旋转函数值，然后通过递推方式计算其他旋转状态下的函数值，从而避免了重复计算，提高了效率。
    ```

---
+++ _01Array/src/_27MaxRotateFunction.java
@@ -0,0 +1,36 @@
+/**
+ * 给定一个长度为 n 的整数数组 nums 。
+ *
+ * 假设 arrk 是数组 nums 顺时针旋转 k 个位置后的数组，我们定义 nums 的 旋转函数  F 为：
+ *
+ * F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1]
+ * 返回 F(0), F(1), ..., F(n-1)中的最大值 。
+ *
+ * 生成的测试用例让答案符合 32 位 整数。
+ */
+public class _27MaxRotateFunction {
+  public int maxRotateFunction(int[] nums) {
+    int n = nums.length;
+    int totalSum = 0;
+    int f = 0;
+
+    // 计算数组的总和以及初始的旋转函数值
+    for (int i = 0; i < n; i++) {
+      totalSum += nums[i];
+      f += i * nums[i];
+    }
+
+    // 初始化最大值为初始的旋转函数值
+    int maxF = f;
+
+    // 计算其他旋转函数的值
+    for (int i = 1; i < n; i++) {
+      // 使用之前的旋转函数值来计算新的旋转函数值
+      f += totalSum - n * nums[n - i];
+      maxF = Math.max(maxF, f);
+    }
+
+    return maxF;
+  }
+}
+
