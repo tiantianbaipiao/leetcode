c20b7a3b8f6b88e16e32b205d9eb375b60b4f639
root <1241062101@qq.com>
2024-08-23 19:34:30 +0800

    ```feat(_15findDuplicates): 实现数组中查找重复元素的功能
    添加一个新的Java类_15findDuplicates，其中包含一个方法findDuplicates，该方法能够识别并返回给定数组中所有重复的元素。该方法通过将数组元素映射到其对应索引位置的标志来高效地检测重复项，将重复元素添加到结果列表中，并返回结果列表。
    ```

---
+++ _01Array/src/_15findDuplicates.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _15findDuplicates {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        // 遍历数组中的每个元素
+        for (int i = 0; i < nums.length; i++) {
+            // 计算当前元素应该在数组中的位置
+            int index = Math.abs(nums[i]) - 1;
+            // 检查该位置上的元素是否已经是负数
+            if (nums[index] < 0) {
+                // 如果已经是负数，则说明该数字已经出现过一次，添加到结果列表
+                result.add(index + 1);
+            } else {
+                // 否则，将该位置上的元素标记为负数
+                nums[index] = -nums[index];
+            }
+        }
+
+        return result;
+    }
+}
