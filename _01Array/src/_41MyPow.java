57f1634f080ce9e20de1c23699773ef44915512a
root <1241062101@qq.com>
2024-08-25 20:44:19 +0800

    ```feat(_41MyPow): 实现自定义的幂计算函数
    不使用系统库函数pow(), 而是通过自定义算法实现计算x的n次方功能。支持负指数运算，通过倒数转换与绝对值取得实现。采用快速幂算法进行高效计算，减少大整数运算中的计算量。
    
    - 实现了_myPow_函数，采用快速幂算法。
    - 处理了n为负数的情况，通过取倒数和绝对值转换问题。
    - 添加了主函数用于示例验证功能。
    ```

---
+++ _01Array/src/_41MyPow.java
@@ -0,0 +1,28 @@
+/**
+ * 编写一个函数来计算 x 的 n 次幂。其中 x 是浮点数，n 是整数。
+ * 我们需要考虑 n 为负数的情况，并且不使用库函数 pow()。
+ */
+public class _41MyPow {
+  public double myPow(double x, int n) {
+    long N = n; // 使用 long 类型来处理大整数
+    if (N < 0) { // 如果指数为负数
+      x = 1 / x; // 取倒数
+      N = -N; // 取绝对值
+    }
+
+    double result = 1.0; // 初始化结果
+    while (N > 0) {
+      if (N % 2 == 1) { // 如果 N 为奇数
+        result *= x; // 直接乘以 x
+      }
+      x *= x; // x 自乘
+      N /= 2; // N 减半
+    }
+
+    return result; // 返回最终结果
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new _41MyPow().myPow(2.00000, -2));
+  }
+}
