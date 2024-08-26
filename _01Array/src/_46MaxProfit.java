17d78c0eee0a2398b587279ac35302d1af8b412c
root <1241062101@qq.com>
2024-08-26 20:33:24 +0800

    ```txtfeat(_46MaxProfit): 新增计算股票最大利润的类
    添加了一个新类 _46MaxProfit，其中包含一个方法 maxProfit，用于计算给定股票每天价格的最大利润，不限制交易次数。
    ```

---
+++ _01Array/src/_46MaxProfit.java
@@ -0,0 +1,22 @@
+/**
+ * 给定一支股票每天的价格，你需要计算出在无限次交易限制下的最大利润。
+ */
+public class _46MaxProfit {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int totalProfit = 0;
+
+        // 从第二天开始遍历价格数组
+        for (int i = 1; i < prices.length; i++) {
+            // 如果今天的股价比昨天高，则说明可以卖出获得利润
+            if (prices[i] > prices[i - 1]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return totalProfit;
+    }
+}
