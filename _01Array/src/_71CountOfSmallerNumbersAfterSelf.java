637968a50f6e4a8c763cb52ae9502b86e158909a
root <1241062101@qq.com>
2024-08-28 09:55:29 +0800

    refactor(计数排序): 使用列表替代数组以支持泛型方法
    将`countSmaller`方法的参数类型从数组改为列表，以便实现泛型方法。此外，调整结果的返回类型为列表，并对main函数进行相应修改以适应这些变更。

--- _01Array/src/_71CountOfSmallerNumbersAfterSelf.java
+++ _01Array/src/_71CountOfSmallerNumbersAfterSelf.java
@@ -1,4 +1,4 @@
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * 给你一个整数数组 nums ，按要求返回一个新数组 counts 。
@@ -7,36 +7,41 @@ import java.util.TreeMap;
 public class _71CountOfSmallerNumbersAfterSelf {
     /**
      * 计算右侧小于当前元素的个数
-     * @param nums 给定的整数数组
-     * @return 结果数组，表示每个元素右侧小于它的元素个数
+     * @param nums 给定的整数列表
+     * @return 结果列表，表示每个元素右侧小于它的元素个数
      */
-    public int[] countSmaller(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        
+    public List<Integer> countSmaller(List<Integer> nums) {
+        int n = nums.size();
+        List<Integer> result = new ArrayList<>(n);
+
         // 使用 TreeMap 来存储元素及其出现次数
         TreeMap<Integer, Integer> treeMap = new TreeMap<>();
 
-        // 从右往左遍历数组
+        // 从右往左遍历列表
         for (int i = n - 1; i >= 0; i--) {
             // 计算当前元素右侧小于它的元素个数
-            result[i] = treeMap.headMap(nums[i]).size();
+            int count = treeMap.headMap(nums.get(i)).size();
 
             // 将当前元素加入 TreeMap
-            treeMap.put(nums[i], treeMap.getOrDefault(nums[i], 0) + 1);
+            treeMap.put(nums.get(i), treeMap.getOrDefault(nums.get(i), 0) + 1);
+
+            // 将结果添加到列表中
+            result.add(count);
         }
+
+        // 反转结果列表，因为是从右往左遍历的
+        Collections.reverse(result);
 
         return result;
     }
 
     public static void main(String[] args) {
         _71CountOfSmallerNumbersAfterSelf solution = new _71CountOfSmallerNumbersAfterSelf();
-        int[] nums = {5, 2, 6, 1};
-        int[] result = solution.countSmaller(nums);
+        List<Integer> nums = Arrays.asList(5, 2, 6, 1);
+        List<Integer> result = solution.countSmaller(nums);
 
         // 输出结果
-        for (int num : result) {
-            System.out.print(num + " ");
-        }
+        System.out.println(result); // 输出 [2, 1, 1, 0]
     }
 }
+
