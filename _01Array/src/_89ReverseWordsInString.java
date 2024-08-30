8cbc676a6db9ecdce56ec4fea4bcc48cac0fd366
root <1241062101@qq.com>
2024-08-30 16:52:55 +0800

    ```markdownfeat(_89ReverseWordsInString): 实现字符串单词翻转方法
    添加一个新类`_89ReverseWordsInString`，其中包含一个公共方法`reverseWords`，用于实现力扣第151题的字符串单词翻转功能。该方法接收一个字符串，去除两端的空白字符，以空格为分隔符分割字符串，然后按照逆序组装单词到一个新的字符串中，并返回翻转后的字符串。
    ```

---
+++ _01Array/src/_89ReverseWordsInString.java
@@ -0,0 +1,28 @@
+/**
+ * 力扣第151题是“翻转字符串里的单词”，要求将输入字符串中的单词顺序翻转
+ */
+public class _89ReverseWordsInString {
+
+    public String reverseWords(String s) {
+        // 去除字符串两端的空白字符
+        s = s.trim();
+
+        // 使用空格分割字符串，忽略多余的空格
+        String[] words = s.split("\\s+");
+
+        // 创建一个新的字符串数组来存储翻转后的单词
+        StringBuilder reversed = new StringBuilder();
+
+        // 从最后一个单词开始向前遍历
+        for (int i = words.length - 1; i >= 0; i--) {
+            // 添加单词，并在每个单词之间添加一个空格
+            reversed.append(words[i]);
+            if (i > 0) {
+                reversed.append(" ");
+            }
+        }
+
+        // 返回翻转后的字符串
+        return reversed.toString();
+    }
+}
