48fea38195fdbe563ffc3b1ad3ba1aa836693641
root <1241062101@qq.com>
2024-09-01 20:56:02 +0800

    新实现的BSTIterator类可对二叉搜索树进行中序遍历
    添加了一个新的BSTIterator类，它公开了二叉搜索树（BST）的中序遍历迭代器。该迭代器在构造时预加载左子树节点到栈中，并在每次调用next()方法时返回下一个最小值节点的值，并继续加载其右子树的左节点到栈。该实现已经过测试，并通过main方法验证了其基本功能。

---
+++ _01Array/src/_116BSTIterator.java
@@ -0,0 +1,65 @@
+import java.util.Stack;
+
+/**
+ * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ */
+public class _116BSTIterator {
+
+    private Stack<TreeNode> stack;
+
+    public _116BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        pushLeftNodes(root);
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+        pushLeftNodes(node.right); // 将右子树的左节点压入栈
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    private void pushLeftNodes(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    // 二叉树节点定义
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 构建二叉搜索树
+        TreeNode root = new TreeNode(7);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(9);
+        root.right.right = new TreeNode(20);
+
+        // 创建迭代器
+        _116BSTIterator iterator = new _116BSTIterator(root);
+
+        // 测试迭代器
+        System.out.println(iterator.next()); // 输出 3
+        System.out.println(iterator.next()); // 输出 7
+        System.out.println(iterator.hasNext()); // 输出 true
+        System.out.println(iterator.next()); // 输出 9
+        System.out.println(iterator.hasNext()); // 输出 true
+        System.out.println(iterator.next()); // 输出 15
+        System.out.println(iterator.hasNext()); // 输出 true
+        System.out.println(iterator.next()); // 输出 20
+        System.out.println(iterator.hasNext()); // 输出 false
+    }
+}
