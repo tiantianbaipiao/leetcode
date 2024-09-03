c1b6492be53c309e1244a2737b9a13326f2c77af
root <1241062101@qq.com>
2024-09-03 21:43:55 +0800

    commit message:``` 添加计算两次交易最大利润的方法
    在`_132MaxProfitTwoTransactions`类中，添加了计算给定股票价格数组的最大利润的方法。此方法考虑了两次交易，与之前支持的单次交易相比，实现了更复杂的交易策略。此外，提供了计算无限次交易的最大利润的辅助方法，为用户提供更多交易策略选择。
    ```

---
+++ _01Array/src/_132MaxProfitTwoTransactions.java
@@ -0,0 +1,49 @@
+public class _132MaxProfitTwoTransactions {
+
+    /**
+     * 计算两次交易的最大利润
+     * @param prices 股票价格数组
+     * @return 最大利润
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        // 定义四个状态变量
+        // f1[i] 表示第 i 天结束时，已经完成一次交易且不持有股票的最大利润
+        // g1[i] 表示第 i 天结束时，已经完成一次交易且持有股票的最大利润
+        // f2[i] 表示第 i 天结束时，已经完成两次交易且不持有股票的最大利润
+        // g2[i] 表示第 i 天结束时，已经完成两次交易且持有股票的最大利润
+
+        int f1 = 0, g1 = Integer.MIN_VALUE;
+        int f2 = 0, g2 = Integer.MIN_VALUE;
+
+        for (int price : prices) {
+            // 更新 g1 和 f1
+            g1 = Math.max(g1, -price); // 第一次买入
+            f1 = Math.max(f1, g1 + price); // 第一次卖出
+
+            // 更新 g2 和 f2
+            g2 = Math.max(g2, f1 - price); // 第二次买入
+            f2 = Math.max(f2, g2 + price); // 第二次卖出
+        }
+
+        return f2; // 最终不持有股票的最大利润
+    }
+
+    /**
+     * 辅助方法：计算无限次交易的最大利润
+     * @param prices 股票价格数组
+     * @return 最大利润
+     */
+    private int maxProfitInfiniteTransactions(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
