382c798ad4b03011fcee7f1b97c7e69a097092c5
root <1241062101@qq.com>
2025-04-21 10:28:57 +0800

    feat(_04MyStudy): reverseList 力扣206 反转链表修改

--- _04MyStudy/src/_04ReverseList.java
+++ _04MyStudy/src/_04ReverseList.java
@@ -1,6 +1,8 @@
 /**
  * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
  * 力扣 206
+ *  1 -> 2  -> 3  -> 4  -> 5
+ *  5 -> 4  -> 3  -> 2  -> 1
  */
 public class _04ReverseList {
   // Definition for singly-linked list.
@@ -20,7 +22,14 @@ public class _04ReverseList {
      // 2. 循环交换
      while (cur != null){
        // 保存下一个节点
+       temp = cur.next;
+       // 当前节点的指针指向上一个节点
+       cur.next = prev;
+       // 移动前一个节点的指针
+       prev = cur;
+       // 移动当前指针
+       cur = temp;
      }
-    return null;
+    return prev;
    }
 }
