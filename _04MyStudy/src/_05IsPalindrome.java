8bb5f7eafc60c539f28ae27d57104b6b124fbc4a
root <1241062101@qq.com>
2025-04-21 11:02:52 +0800

    feat(_04MyStudy): isPalindrome 力扣234 回文链表

---
+++ _04MyStudy/src/_05IsPalindrome.java
@@ -0,0 +1,30 @@
+/**
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ * 力扣 234 回文链表 往前读 往后读都是一样的
+ * 1 - 2 - 2- 1
+ *
+ */
+public class _05IsPalindrome {
+  // Definition for singly-linked list.
+  public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+  // 回文链表 快慢指针
+  public boolean isPalindrome(ListNode head) {
+    // 主要思想是将链表分为两部分 反转第二部分后再进行比较
+    // 如果是空链表或者只有一个节点的链表都是回文链表
+    if(head == null || head.next ==null){
+      return true;
+    }
+    // 1. 快慢指针法找到需要反转的位置(慢指针slow走一步，快指针fast走两步)
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next == null){
+    }
+    return true;
+  }
+}
